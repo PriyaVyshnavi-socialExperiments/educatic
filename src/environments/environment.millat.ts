f8dec84ce04052f581e59e21305b9e4d1435c9ae
zencondchaskar <dattatraya.chaskar@zconsolutions.com>
2020-10-14 14:28:05 +0530

    millat project setups

---
+++ src/environments/environment.millat.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    appVersion: require('../../package.json').version,
+    production: true,
+    apiBaseUrl: 'https://millat-api.azurewebsites.net/api',
+    appInsightsKey: 'd17999d5-f224-430e-8673-7a549dd41010',
+    sqlLiteDBName: 'millat',
+    ImageSource: 'millat',
+    PageTitle: 'Millat Model High School',
+  };
