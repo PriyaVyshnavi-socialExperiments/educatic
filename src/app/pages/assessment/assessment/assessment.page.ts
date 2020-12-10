0f6091e3856b15457a048ae2f1488c82268231af
zencondchaskar <dattatraya.chaskar@zconsolutions.com>
2020-12-10 16:06:23 +0530

    removed school level filteration in assessments

--- src/app/pages/assessment/assessment/assessment.page.ts
+++ src/app/pages/assessment/assessment/assessment.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { IAssessment } from 'src/app/_models/assessment';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IAssessment, IQuestion } from 'src/app/_models/assessment';
 import { QuestionType } from 'src/app/_models/question-type';
 
 @Component({
@@ -11,12 +11,29 @@ import { QuestionType } from 'src/app/_models/question-type';
 export class AssessmentPage implements OnInit {
   title: string;
   assessment: IAssessment;
-  options = [1,2,3,4];
-  constructor( private activatedRoute: ActivatedRoute) { }
+  options = [1, 2, 3, 4];
+  visibleQuestion = 0;
+  questionCount = 0;
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
     this.title = this.activatedRoute.snapshot.paramMap.get('subject');
     this.assessment = history.state.assessment as IAssessment;
+    this.questionCount = this.assessment.assessmentQuestions.length - 1;
+    if (!this.assessment) {
+      this.router.navigateByUrl(`assessment/quizzes/${this.title}`);
+    }
+  }
+
+  visibleNext(question: IQuestion, index: number) {
+    return true;
+  }
+
+  selectedAnswerOption(event) {
+    console.log('selectedAnswerOption ', event.target.value);
   }
 
 }
